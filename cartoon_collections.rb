56d823743eb334aa09eaed3e2db4ca510fbde0e4
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-18 13:30:31 -0700

    I get it now

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(arr)
+def roll_call_dwarves(dwarves)
   # Your code here
-  arr.map.with_index(1) do |dwarf, num|
+  dwarves.map.with_index(1) do | dwarf, num|
     puts "#{num}. #{dwarf}"
   end
 end
@@ -12,13 +12,11 @@ end
 
 def long_planeteer_calls(calls)
   # Your code here
-  calls.any? {|call| call.length > 4}
+  calls.any? { |call| call.length > 4}
 end
 
 def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.find do |cheese|
-    cheese_types.include?(cheese)
-  end
+  cheese_types.find {|cheese| arr.include?(cheese)}
 end
